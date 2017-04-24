2872c2d840986f0cefb1fea7b9ad8683ab489eff
Nathaniel <nat.kitzke@gmail.com>
2017-04-23 22:37:52 -0400

    reconfigure next and package scripts

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ app.prepare()
 
   server.listen(3000, (err) => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    // console.log('> Ready on http://localhost:3000')
   })
 })
 .catch((ex) => {
-  console.error(ex, ex.stack)
+  console.error(ex.stack)
   process.exit(1)
 })
